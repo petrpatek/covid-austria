e7298f78dfc7806bbd3dc3c6a1e552d6730dd37a
Petr Patek <petrpatekml@gmail.com>
2020-03-17 16:02:11 +0100

    utc

--- src/main.js
+++ src/main.js
@@ -80,7 +80,7 @@ Apify.main(async () => {
         deathByRegion: extracted.deathByRegion,
         sourceUrl: url,
         lastUpdatedAtSource: extracted.lastUpdated,
-        lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 1, now.getMinutes())).toISOString(),
+        lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())).toISOString(),
         readMe: 'https://apify.com/petrpatek/covid-austria',
     };
 
